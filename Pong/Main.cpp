b6ea7eefeae80de7087f99355bed7d2b3adb42f9
Bautista <baumancastignani@hotmail.com>
2023-09-15 11:13:47 -0300

    Main y configuracion

---
+++ Pong/Main.cpp
@@ -0,0 +1,20 @@
+﻿#include "sl.h"
+
+int main()
+{
+	// set up our window and a few resources we need
+	slWindow(400, 400, "Simple SIGIL Example", false);
+	
+
+	while (!slShouldClose() && !slGetKey(SL_KEY_ESCAPE))
+	{
+		slSetForeColor(1, 0, 0, 1);
+		slRectangleFill(200, 240, 100, 7);
+
+	
+
+		slRender();
+	}
+	slClose();
+	return 0;
+}
